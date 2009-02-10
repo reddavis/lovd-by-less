c7df99c675b76b2ab05c5c12a67f30a3d15dbfcf
Red Davis <reddavis@gmail.com>
2009-02-10 22:25:44 +0000

    embedit doesnt require curb as we are not uploading images

--- lib/embedit_ruby.rb
+++ lib/embedit_ruby.rb
@@ -6,9 +6,6 @@ require 'cgi'
 $KCODE = "u"
 
 require 'parser'
-require 'image'
 require 'url'
 
 #puts EmbeditRuby::Url.new("http://revision3.com/internetsuperstar/ijustine/").html
-
-#puts EmbeditRuby::Image.new('path/to/there.png', 1111, 42222, :height => 10, :width => 100).valid?
